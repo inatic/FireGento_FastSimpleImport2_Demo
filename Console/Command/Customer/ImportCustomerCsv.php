e70a3440a675946ec3136bc34c613452379c2d40
Jim van Doren <jim.vandoren@gmail.com>
2019-04-08 15:25:24 +0000

    CSV import for customers and addresses

---
+++ Console/Command/Customer/ImportCustomerCsv.php
@@ -0,0 +1,90 @@
+<?php
+namespace FireGento\FastSimpleImportDemo\Console\Command\Customer;
+
+use Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use FireGento\FastSimpleImportDemo\Console\Command\AbstractImportCommand;
+use Magento\Framework\App\ObjectManagerFactory;
+use Magento\ImportExport\Model\Import;
+use League\Csv\Reader;
+use League\Csv\Statement;
+
+class ImportCustomerCsv extends AbstractImportCommand
+{
+    const IMPORT_FILE = "customer.csv";
+
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\ReadFactory
+     */
+    private $readFactory;
+    /**
+     * @var DirectoryList
+     */
+    private $directory_list;
+
+
+    /**
+     * Constructor
+     *
+     * @param ObjectManagerFactory $objectManagerFactory
+     */
+    public function __construct(
+        ObjectManagerFactory $objectManagerFactory,
+        \Magento\Framework\Filesystem\Directory\ReadFactory $readFactory,
+        \Magento\Framework\App\Filesystem\DirectoryList $directory_list
+    )
+    {
+
+        parent::__construct($objectManagerFactory);
+
+
+        $this->readFactory = $readFactory;
+
+        $this->directory_list = $directory_list;
+    }
+
+    protected function configure()
+    {
+
+        $this->setName('fastsimpleimportdemo:customers:importcustomercsv')
+            ->setDescription('Import Customers from CSV');
+        $this->setBehavior(Import::BEHAVIOR_APPEND);
+        $this->setEntityCode('customer');
+
+        parent::configure();
+    }
+
+    /**
+     * @return array
+     */
+    protected function getEntities()
+    {
+        $csvIterationObject = $this->readCSV();
+        $data = array();
+        // Do mapping here:
+        foreach($csvIterationObject as $row){
+            $data[]  = $row;
+        }
+        //  Mapping end
+        //var_dump($data);
+        //die();
+        return $data;
+    }
+
+    protected function readCSV()
+    {
+        $csvObj = Reader::createFromString($this->readFile(static::IMPORT_FILE));
+        $csvObj->setDelimiter(',');
+        $csvObj->setHeaderOffset(0);
+        $results = (new Statement())->process($csvObj);
+        return $results;
+
+    }
+
+    protected function readFile($fileName)
+    {
+        $path = $this->directory_list->getRoot();
+        $directoryRead = $this->readFactory->create($path);
+        return $directoryRead->readFile($fileName);
+    }
+}
